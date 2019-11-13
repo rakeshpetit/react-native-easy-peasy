d70a3a830e1c6dfd7cc6fde9e4f4abf86b841089
Rakesh Arunachalam <rakesh@thoughtbot.com>
2019-11-13 12:24:14 +0000

    Add todo using easy-peasy action hooks

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import {View, Button, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Button, Text, TextInput} from 'react-native';
 import {useStoreActions, useStoreState} from 'easy-peasy';
+
 const Home = () => {
   const {todos} = useStoreState(state => state.todoList);
-  console.log(todos);
+  const addTodo = useStoreActions(actions => actions.todoList.addTodo);
+  const [todoItem, setTodoItem] = useState('');
+  const addTodoToList = () => {
+    if (todoItem) {
+      addTodo({text: todoItem, completed: false});
+      setTodoItem('');
+    }
+  };
   return (
     <View>
       <Text style={{textAlign: 'center'}}>My Todos</Text>
       {todos.map((todo, index) => (
-        <Text>{`${index+1}. ${todo.text}`}</Text>
+        <Text>{`${index + 1}. ${todo.text}`}</Text>
       ))}
+      <TextInput
+        style={{
+          padding: 20,
+          marginHorizontal: 20,
+          backgroundColor: 'lightgray',
+        }}
+        value={todoItem ? todoItem : ''}
+        onChangeText={text => setTodoItem(text)}
+      />
+      <Button onPress={() => addTodoToList()} title="Add" />
     </View>
   );
 };
