5f16418f0c2a682838354837a616f2b66d51b4e3
Rakesh Arunachalam <rakesh@thoughtbot.com>
2019-11-13 13:42:21 +0000

    integrate with sagas

--- store/sagas/index.js
+++ store/sagas/index.js
@@ -1,14 +1,22 @@
-import {call, delay} from 'redux-saga/effects';
-import store from '../';
+import {takeEvery, call, put, delay} from 'redux-saga/effects';
 
 export function* rootSaga() {
   console.log('Hello Sagas!');
   yield call(addTodo);
+  yield takeEvery('@action.todoList.removeTodo', removeTodoAudit);
+}
+
+function* removeTodoAudit(payload) {
+  console.log('A todo was removed', payload);
 }
 
 function* addTodo() {
-  yield delay(1000);
-  store
-    ? store.dispatch.todoList.addTodo({text: 'saga', completed: 'false'})
-    : null;
+  yield delay(2000);
+  yield put({
+    type: '@action.todoList.addTodo',
+    payload: {
+      text: 'saga action',
+      completed: 'false',
+    },
+  });
 }
